429b59937e82614e3fc40e102d963fbf1f85e1ce
lenalog13 <56520937+lenalog13@users.noreply.github.com>
2023-01-18 22:36:03 +0300

    create struct

--- PersonalQuiz/ViewControllers/QuestionViewController.swift
+++ PersonalQuiz/ViewControllers/QuestionViewController.swift
@@ -8,12 +8,68 @@
 import UIKit
 
 final class QuestionViewController: UIViewController {
-
+    
+    // MARK: - IBOutlets
+    
+    @IBOutlet var questionLable: UILabel!
+    @IBOutlet var questionProgressView: UIProgressView!
+    
+    @IBOutlet var singleStackView: UIStackView!
+    @IBOutlet var singleButtons: [UIButton]!
+    
+    @IBOutlet var multipleStackView: UIStackView!
+    @IBOutlet var multipleLabels: [UILabel]!
+    @IBOutlet var multipleSwitches: [UISwitch]!
+    
+    @IBOutlet var rangedStackView: UIStackView!
+    @IBOutlet var rangedLables: [UILabel]!
+    @IBOutlet var rangedSlider: UISlider!
+    
+    // MARK: - Private Properties
+    
+    private let questions = Question.getQuestions()
+    private var answerChosen: [Answer] = []
+    private var questionIndex = 0
+    
+    private var currentAnswers: [Answer] {
+        questions[questionIndex].answers
+    }
+    
+    //MARK: - Override Func
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI()
     }
-
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+    
+    // MARK: - IBActions
+    
+    @IBAction func singleAnswerButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func multipleAnswerButtonPressed() {
+    }
+    
+    @IBAction func rengedAnswerButtonPressed() {
+    }
+    
 }
+
+    // MARK: - User Interface
+
+extension QuestionViewController {
+    private func updateUI() {
+        
+        
+        
+        
+    }
+    
+    
+}
+
 
--- PersonalQuiz/ViewControllers/ResultViewController.swift
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -8,11 +8,24 @@
 import UIKit
 
 final class ResultViewController: UIViewController {
-
+    
+    //MARK: - IBOutlet
+    
+    @IBOutlet var resultLebel: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+    
+    //MARK: - Private Properties
+    
+    
+    //MARK: - Ovveride Func
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    //MARK: - IBActions
 
+    @IBAction func doneButtonPressed(_ sender: Any) {
+    }
 }
