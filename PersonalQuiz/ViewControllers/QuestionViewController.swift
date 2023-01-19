9f3a40d9078f46070d356555d0c22c38dd111717
lenalog13 <56520937+lenalog13@users.noreply.github.com>
2023-01-19 22:31:58 +0300

    writing logic

--- PersonalQuiz/ViewControllers/QuestionViewController.swift
+++ PersonalQuiz/ViewControllers/QuestionViewController.swift
@@ -23,7 +23,14 @@ final class QuestionViewController: UIViewController {
     
     @IBOutlet var rangedStackView: UIStackView!
     @IBOutlet var rangedLables: [UILabel]!
-    @IBOutlet var rangedSlider: UISlider!
+    @IBOutlet var rangedSlider: UISlider! {
+        didSet {
+            let answerCount = Float(currentAnswers.count - 1)
+            
+            rangedSlider.maximumValue = answerCount
+            rangedSlider.value = answerCount / 2
+        }
+    }
     
     // MARK: - Private Properties
     
@@ -43,18 +50,32 @@ final class QuestionViewController: UIViewController {
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
+        guard let resultVS = segue.destination as? ResultViewController else {return}
+        resultVS.answerChosen = answerChosen
     }
     
     // MARK: - IBActions
     
     @IBAction func singleAnswerButtonPressed(_ sender: UIButton) {
+        guard let currentIndex = singleButtons.firstIndex(of: sender) else {return}
+        let currentAnswer = currentAnswers[currentIndex]
+        answerChosen.append(currentAnswer)
+        nextQuesrion()
     }
     
     @IBAction func multipleAnswerButtonPressed() {
+        for (multiple, answer) in zip(multipleSwitches, currentAnswers){
+            if multiple.isOn {
+                answerChosen.append(answer)
+            }
+        }
+        nextQuesrion()
     }
     
     @IBAction func rengedAnswerButtonPressed() {
+        let index = lrintf(rangedSlider.value)
+        answerChosen.append(currentAnswers[index])
+        nextQuesrion()
     }
     
 }
@@ -63,12 +84,65 @@ final class QuestionViewController: UIViewController {
 
 extension QuestionViewController {
     private func updateUI() {
+        for stackView in [singleStackView, multipleStackView, rangedStackView] {
+            stackView?.isHidden = true
+        }
         
+        let currentQuestion = questions[questionIndex]
         
+        title = "Вопрос №\(questionIndex + 1) из \(questions.count)"
+        questionLable.text = currentQuestion.title
         
+        let totalProgress = Float(questionIndex) / Float(questions.count)
+        questionProgressView.setProgress(totalProgress, animated: true)
         
+        showCurrentAnswers(for: currentQuestion.type)
+    
     }
     
+    private func showCurrentAnswers(for type: ResponseType) {
+        switch type {
+        case .single:
+            showSingleStackView(with: currentAnswers)
+        case .multiple:
+            showMultipleStackView(with: currentAnswers)
+        case .ranged:
+            showRangedStackView(with: currentAnswers)
+        }
+    }
+    
+    private func showSingleStackView(with answers: [Answer]) {
+        singleStackView.isHidden = false
+        
+        for (button, answer) in zip(singleButtons, answers) {
+            button.setTitle(answer.title, for: .normal)
+        }
+    }
+    
+    private func showMultipleStackView(with answers: [Answer]) {
+        multipleStackView.isHidden = false
+        
+        for (label, answer) in zip(multipleLabels, answers) {
+            label.text = answer.title
+        }
+    }
+    
+    private func showRangedStackView(with answers: [Answer]) {
+        rangedStackView.isHidden = false
+        
+        rangedLables.first?.text = answers.first?.title
+        rangedLables.last?.text = answers.last?.title
+    }
+    
+    private func nextQuesrion() {
+        questionIndex += 1
+        
+        if questionIndex < questions.count {
+            updateUI()
+        } else {
+            performSegue(withIdentifier: "showResult", sender: nil)
+        }
+    }
     
 }
 
--- PersonalQuiz/ViewControllers/ResultViewController.swift
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -14,18 +14,43 @@ final class ResultViewController: UIViewController {
     @IBOutlet var resultLebel: UILabel!
     @IBOutlet var descriptionLabel: UILabel!
     
-    //MARK: - Private Properties
+    //MARK: - Properties
     
+    var answerChosen: [Answer]!
+    private var result: AnimalType!
     
     //MARK: - Ovveride Func
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        navigationItem.hidesBackButton = true
+        summarizing()
+        resultLebel.text = "Вы - \(result.rawValue)"
+        descriptionLabel.text = result.definition
     }
 
     //MARK: - IBActions
 
     @IBAction func doneButtonPressed(_ sender: Any) {
+        dismiss(animated: true)
+    }
+}
+
+extension ResultViewController {
+    private func summarizing() {
+        let catCount = answerChosen.filter { $0.type == .cat }.count
+        let dogCount = answerChosen.filter { $0.type == .dog }.count
+        let rabbitCount = answerChosen.filter { $0.type == .rabbit }.count
+        let tartleCount = answerChosen.filter { $0.type == .turtle }.count
+        
+        if catCount >= dogCount, catCount >= rabbitCount, catCount >= tartleCount {
+            result = .cat
+        } else if dogCount >= rabbitCount, dogCount >= tartleCount {
+            result = .dog
+        } else if rabbitCount >= tartleCount {
+            result = .rabbit
+        } else {
+            result = .turtle
+        }
     }
 }
